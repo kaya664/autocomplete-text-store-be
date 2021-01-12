b8f120af842e98d257936b552658fbcc8b77bb3d
Ahmet-Kaya <kayastd@gmail.com>
2021-01-12 13:32:47 +0300

    fix annotaion

--- src/main/java/com/kaya/textstorecontroller/service/TextStoreService.java
+++ src/main/java/com/kaya/textstorecontroller/service/TextStoreService.java
@@ -1,11 +1,13 @@
 package com.kaya.textstorecontroller.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.datastax.oss.driver.api.core.uuid.Uuids;
 import com.kaya.textstorecontroller.entity.Phrase;
 import com.kaya.textstorecontroller.repository.PhraseRepository;
 
+@Service
 public class TextStoreService {
 	@Autowired
 	PhraseRepository phraseRepository;
